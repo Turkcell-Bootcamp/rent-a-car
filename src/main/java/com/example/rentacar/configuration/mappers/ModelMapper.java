6105b0bd85baf07368041adfe923828fe42972b9
emrahyilm4z <emrahyilm4z@gmail.com>
2023-03-29 02:03:01 +0300

    Integrated model mapper

---
+++ src/main/java/com/example/rentacar/configuration/mappers/ModelMapper.java
@@ -0,0 +1,12 @@
+package com.example.rentacar.configuration.mappers;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapper {
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
+}
